1aafb39f46d673103d65f1903775a17012c3216d
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-06-19 08:28:36 -0300

    I created a function resize_image to resize the favicon image to 32px and called this function in the SiteSetup model.

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from site_setup.utils.model_validators import validate_png
+from site_setup.utils.images import resize_image
 
 class MenuLink(models.Model):
     class Meta:
@@ -37,7 +39,19 @@ class SiteSetup(models.Model):
         upload_to="assets/favicon/%Y/%m/",
         blank=True,
         default="",
+        validators=[validate_png],
     )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self) -> str:
         return self.title
