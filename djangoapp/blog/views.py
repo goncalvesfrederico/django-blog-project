bfb392415de51d7a652a498c2d98e6bcc887c444
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-07-05 12:13:00 -0300

    I changed my page() and post() Function-Based View to PageDetailView() and PostDetailView Class-Based View. Both CBV are  inheriting from the DetailView.

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,10 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
+from django.shortcuts import redirect
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404, HttpRequest, HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post, Page
 
 PER_PAGE = 9
@@ -25,41 +24,25 @@ class PostListView(ListView):
         return context
 
 
-def post(request, slug):
-    post_obj = Post.objects.get_published().filter(slug=slug).first()
+class PostDetailView(DetailView):
+    template_name = "blog/pages/post.html"
+    queryset = Post.objects.get_published()
 
-    if post_obj is None:
-        raise Http404()
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = f"{self.object.title} Post - "
+        return context
+
+
+class PageDetailView(DetailView):
+    template_name = "blog/pages/page.html"
+    queryset = Page.objects.get_published()
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = f"{self.object.title} Page - "
+        return context
     
-    page_title = f"{post_obj.title} Post - "
-
-    return render(
-        request,
-        "blog/pages/post.html",
-        {
-            "post": post_obj,
-            "page_title": page_title,
-        }
-    )
-
-def page(request, slug):
-    page_obj = Page.objects.get_published().filter(slug=slug).first()
-
-    if page_obj is None:
-        raise Http404()
-    
-    page_title = f"{page_obj.title} Page - "
-
-    return render(
-        request,
-        "blog/pages/page.html",
-        {
-            "page": page_obj,
-            "page_title": page_title,
-
-        }
-    )
-
 
 class CreatedByListView(PostListView):
     def __init__(self, **kwargs: Any) -> None:
@@ -88,7 +71,7 @@ class CreatedByListView(PostListView):
         return qs
     
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        ctx = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         user = self._temp_context["user"]
         user_full_name = user.username
 
@@ -96,13 +79,13 @@ class CreatedByListView(PostListView):
             user_full_name = f"{user.first_name} {user.last_name} "
         
         page_title = f"Posts by {user_full_name} - "
-        ctx.update(
+        context.update(
             {
                 "page_title": page_title,
             }
         )
 
-        return ctx
+        return context
 
 
 class CategoryListView(PostListView):
